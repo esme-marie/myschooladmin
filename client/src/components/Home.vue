181fb10e434fea8d083fa83a8d0cf4396b27b929
Esme-Marie <esmemarie.sultan@gmail.com>
2021-01-10 23:55:19 +0800

    Create Home Page

---
+++ client/src/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="Home">
+        <section class="hero is-dark">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title" style="text-align: left; padding: 100px">Welcome<br/>Teacher Awesome!</h1>
+            </div><br/><br/><br/>
+            <div>
+                <h5>Grades submission for Mid-Semester 1 | 2020-2021 is open , deadline for submission is by <span style="color: red;">31st January 2021</span></h5><br/>
+                <h5><span style="background-color: #FFE87C; padding: 11px;">Make sure to read the notice below before proceeding to the Grades Page!</span></h5>
+            </div>    
+        </div><br/><br/>
+        <div style="border: 2px solid salmon; border-radius: 11px; padding: 22px">
+            <h4><span><i style="color: red;" class="fa fa-exclamation"></i></span> Kindly ensure that you are uploading the <span style="color: red;">CORRECT FILE</span> because there is <span style="color: red;">NO DELETE</span> button for data protection purposes. <span style="background-color: #81D8D0;">We’re too SCHOOL for Cool, that’s how we ROLL! 🤓</span>  Incase for whatever reason you still made an Oopsie! 😱  please contact our administrator with your supporting documents</h4>
+            <h4><span style="background-color: #F9966B;">We’re still COOL like that! 😎</span></h4>
+        </div><br/>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home",
+    // components: {
+    //     EventList,
+    // },
+};
+</script>
+<style lang="scss" scoped>
+.hero {
+    text-align: center;
+    background-image: url("https://www.pixartprinting.co.uk/blog/wp-content/uploads/2020/01/autopubblicazione.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 400px;
+}
+.hero-body .title {
+    text-shadow: 4px 4px 4px #E67451;
+    padding: 40px 0 20px 0;
+    font-size: 60px;
+}
+// .subtitle {
+//     text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+//     font-size: 30px;
+// }
+// .button-block {
+//     text-align: center;
+//     margin-left: auto;
+//     margin-right: auto;
+//     width: 100%;
+//     position: absolute;
+//     bottom: -150px;
+//     .button {
+//     margin-right: 50px;
+//     padding-left: 50px;
+//     padding-right: 50px;
+//     }
+//     .welcome {
+//         width: 400px;
+//         padding: 10px;
+//         margin-left: auto;
+//         margin-right: auto;
+//     }
+// }
+// .is-xl {
+//     font-size: 1.7rem;
+// }
+</style>

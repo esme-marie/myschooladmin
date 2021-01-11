4f9146ee6e8d34ab32f7cfc989fbef7f1c765cc3
Esme-Marie <esmemarie.sultan@gmail.com>
2021-01-11 16:12:37 +0800

    Code Clean Up Grades.vue

--- client/src/components/Grades.vue
+++ client/src/components/Grades.vue
@@ -199,8 +199,8 @@ export default {
       }
     },
     reloadPage() {
-    window.location.reload()
-    console.log("clicked!")
+      window.location.reload()
+      console.log("clicked!")
     }
   }
 };
